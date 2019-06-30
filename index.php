e4a47a9d412528968406f2c3cfa71d7d5a356d84
Adam Fischer <gbg_adam@live.se>
2019-06-30 17:13:32 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SteamID.se</title>
+	<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+	<link href="https://fonts.googleapis.com/css?family=Noto+Sans&display=swap" rel="stylesheet">
+    <link href="css/index.css" rel="stylesheet" />
+    <script>
+    	$(document).ready(function(){
+    		
+    	});
+    </script>
+</head>
+<body>
+
+  <!---- Navbar ----->
+  <!---- End of navbar ----->
+
+  <!----Start of contentbox ----->
+  <div id="contentbox">
+        <div id="searchbar">
+        	<div class="searchbarpos">
+        		<input type="text" placeholder="Steamid, steam64id, steam32id, profile url" class="searchtext" />
+        		<input type="submit" class="searchbarbtn" value="Sök" />
+        	</div>	
+        </div>
+
+        <div class="userinfobox">
+        	<div class="searchbarpos">
+        		<h3> Steam Id Finder - is the simplest way to find steam id online by anything you know: </h3>
+        		<ul>
+        			<li> Steamid </li>
+        			<li> accountid </li>
+        	    </ul>
+        	</div>	
+        </div>	
+   </div>
+
+</body>
+</html>
---
+++ links.php
@@ -0,0 +1,4 @@
+Användarinformation: https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=4FD1CA494FC40FDA38140D4B288DCFEA&steamids=76561198077278446
+
+CSGO Stats: http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=730&key=4FD1CA494FC40FDA38140D4B288DCFEA&steamid=76561198077278446
+
